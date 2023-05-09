16a4ea935d971b19311e8765877dc07c0a1f7b01
Filipe Fernandes <filipeferibeiro@outlook.com>
2023-05-08 21:24:42 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Filipe Fernandes on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
